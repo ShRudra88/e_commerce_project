1001ad2d49fbff9c74c20c6570e740b4d521214e
ShRudra88 <salhawathosenrudra503@gmail.com>
2024-02-01 19:50:39 +0600

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,13 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// layer first - > layer (data, ui, domain)
+// feature first - login (layer), cart(layer)
+
+// good code, bad code
+// bad code, code smell
+// good code - reaqdibility , rousobility, extend
+// refactoring - process - bad - clean - good
+// kiss - keep it simple, stupid
